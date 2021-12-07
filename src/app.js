24a6b04a9f0e6ff47d59e00b1a969868e5f8a85f
Marcelo Olivera <olivera.m.et13@gmail.com>
2021-12-07 15:49:04 -0300

    Marcelo Olivera - Ruta , controlador del menu terminado ,  cambio a formato EJS en proceso

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 
 app.set('view engine', 'ejs');
-app.set('views', 'src/views');
+app.set('views', 'views');
 
 
 
--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,14 +1,52 @@
-
-
 //**Rutas de acceso disponnible para el usuario  */
 const controllerIndex = {
     home: (req,res) =>{
         res.render('./menu/index');
     },
     menu: (req,res) =>{
-        res.render('./menu/menu');
+        res.render('./menu/menu', {categorias : categoria} )
     },
 } 
 
-
 module.exports =  controllerIndex;
+
+
+//** */
+const categoria = [
+    {
+        id: 1,
+        categoria: 'Hamburguesa',
+        image: 'hamburguesa1.jpg',
+        textoAlternativo: 'Menu de Hamburguesas',
+    },
+    {
+        id: 2,
+        categoria: 'Pizza',
+        image: 'pizza.jpeg',
+        textoAlternativo: 'Seccion de pizzas',
+    },
+    {
+        id: 3,
+        categoria: 'Ravioles',
+        image: 'ravioles.jpeg',
+        textoAlternativo: 'Seccion de Ravioles',
+    },
+    {
+        id: 4,
+        categoria: 'Empanadas',
+        image: 'empanadas.jpeg',
+        textoAlternativo: 'Seccion de empanadas',
+    },
+    {
+        id: 5,
+        categoria: 'Asado',
+        image: 'carneasada.jpeg',
+        textoAlternativo: 'Seccion de Asado',
+    },
+    {
+        id: 6,
+        categoria: 'Bebidas',
+        image: 'coctel.jpg',
+        textoAlternativo: 'Seccion de bebidas',
+    }
+]
